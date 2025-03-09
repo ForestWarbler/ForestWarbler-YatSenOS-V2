7ba404b1bb585daa8ac22686dad6aaae05ab9df3
Warbler Forest <warblerforest@Warblers-Laptop.local>
2025-03-09 09:55:57 +0800

    0x00 Finished

--- pkg/boot/src/main.rs
+++ pkg/boot/src/main.rs
@@ -1,4 +1,29 @@
 #![no_std]
 #![no_main]
 
-// TODO
+#[macro_use]
+extern crate log;
+extern crate alloc;
+
+use core::fmt::Write;
+use core::arch::asm;
+use uefi::{Status, entry};
+use uefi::prelude::*;
+
+#[entry]
+fn efi_main() -> Status {
+    uefi::helpers::init().expect("Failed to initialize utilities");
+    log::set_max_level(log::LevelFilter::Info);
+
+    let std_num = 23336338;
+
+    loop {
+        info!("Hello World from UEFI bootloader! @ {}", std_num);
+
+        for _ in 0..0x10000000 {
+            unsafe {
+                asm!("nop");
+            }
+        }
+    }
+}
